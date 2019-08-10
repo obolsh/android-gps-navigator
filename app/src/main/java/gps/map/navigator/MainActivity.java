54ea53f81d346173c2c024d45fde171342b1034c
Oleksii Bolshakov <obolsh@Obolsh.local>
2019-08-10 23:32:17 +0300

    Initial commit

---
+++ app/src/main/java/gps/map/navigator/MainActivity.java
@@ -0,0 +1,13 @@
+package gps.map.navigator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
