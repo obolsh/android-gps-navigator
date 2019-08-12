31ece6c82daf16a3a992766a376e1896e2bdd844
Oleksii Bolshakov <obolsh@Obolsh.local>
2019-08-12 04:58:42 +0300

    Add menu for button nav drawer

--- app/src/main/java/gps/map/navigator/MainActivity.java
+++ app/src/main/java/gps/map/navigator/MainActivity.java
@@ -1,14 +1,19 @@
 package gps.map.navigator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 
+import gps.map.navigator.view.ui.BottomNavigationDrawerFragment;
+
 public class MainActivity extends AppCompatActivity {
     BottomAppBar bottomAppBar;
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +21,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         bottomAppBar = findViewById(R.id.bottom_app_bar);
         setSupportActionBar(bottomAppBar);
+        fragmentManager = getSupportFragmentManager();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.bottomappbar_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                BottomNavigationDrawerFragment bottomNavDrawerFragment = new BottomNavigationDrawerFragment();
+                bottomNavDrawerFragment.show(fragmentManager, bottomNavDrawerFragment.getTag());
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
