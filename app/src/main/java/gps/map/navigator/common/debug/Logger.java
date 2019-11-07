6ef3fea4f2d8aa205fd3b632314c20532213ef0b
obolsh <obolsh@gmail.com>
2019-11-07 23:17:03 +0200

    Add description to interface

--- app/src/main/java/gps/map/navigator/common/debug/Logger.java
+++ app/src/main/java/gps/map/navigator/common/debug/Logger.java
@@ -1,10 +1,24 @@
 package gps.map.navigator.common.debug;
 
 public interface Logger {
-
+    /**
+     * print message in logcat if its debug build.
+     *
+     * @param message - text to print.
+     */
     void debug(String message);
 
+    /**
+     * print error message in logcat.
+     *
+     * @param message - text to print.
+     */
     void error(String message);
 
+    /**
+     * print throwable in logcat.
+     *
+     * @param throwable - text to print.
+     */
     void error(Throwable throwable);
 }
