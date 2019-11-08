e0c97f81cea1410d69663129c94a50146f8eebf7
obolsh <obolsh@gmail.com>
2019-11-08 23:31:33 +0200

    Add tests

---
+++ app/src/test/java/androidx/fragment/app/Fragment.java
@@ -0,0 +1,26 @@
+package androidx.fragment.app;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return null;
+    }
+
+    public void onAttach(Context context) {
+
+    }
+
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+
+    }
+}
---
+++ app/src/test/java/com/google/android/material/bottomsheet/BottomSheetDialogFragment.java
@@ -0,0 +1,8 @@
+package com.google.android.material.bottomsheet;
+
+import androidx.fragment.app.Fragment;
+
+public class BottomSheetDialogFragment extends Fragment {
+
+
+}
