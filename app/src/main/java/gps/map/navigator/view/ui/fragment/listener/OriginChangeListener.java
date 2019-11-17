a05e46b4380e08a252f2685d19ca138d60edd7d5
obolsh <obolsh@gmail.com>
2019-11-17 17:56:45 +0200

    Filter origin & destination match

--- app/src/main/java/gps/map/navigator/view/ui/fragment/listener/OriginChangeListener.java
+++ app/src/main/java/gps/map/navigator/view/ui/fragment/listener/OriginChangeListener.java
@@ -6,12 +6,15 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import gps.map.navigator.model.interfaces.Cache;
 import gps.map.navigator.model.interfaces.IMapPlace;
 import gps.map.navigator.model.interfaces.PlaceProxyListener;
 
 public class OriginChangeListener implements PlaceProxyListener {
     @Inject
     ISwipeRoute swipeRoute;
+    @Inject
+    Cache cache;
 
     @Inject
     OriginChangeListener() {
@@ -19,11 +22,19 @@ public class OriginChangeListener implements PlaceProxyListener {
 
     @Override
     public void onPlaceLocated(@NonNull IMapPlace mapPlace) {
-        swipeRoute.setOnlyOrigin(mapPlace);
+        if (!placesAreTheSame(mapPlace, cache.getLastDestination())) {
+            swipeRoute.setOnlyOrigin(mapPlace);
+        }
     }
 
     @Override
     public void onPlacesLocated(@NonNull List<IMapPlace> mapPlaces) {
 
+    }
+
+    private boolean placesAreTheSame(IMapPlace place, IMapPlace comparing) {
+        return place != null && comparing != null
+                && place.getLongitude() == comparing.getLongitude()
+                && place.getLatitude() == comparing.getLatitude();
     }
 }
